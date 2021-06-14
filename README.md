fb6dd06aa7b8a8122dd83f5d235a2ea8cc02b8d5
TATSUNO Yasuhiro <ytatsuno.jp@gmail.com>
2021-06-14 15:41:14 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# AWS SSO Portal Helper
+
+## Install
+
+TODO: Add link to Chrome WebStore
+
+## Develop
+
+1. `npm install`
+2. `npm run start:chrome` or `npm run start:firefox`
+3. Open `https://***.awsapps.com/start/`
+4. Edit source code and reload
---
+++ src/apps-visibility.js
@@ -0,0 +1,100 @@
+'use strict'
+
+let StorageLocal
+try {
+  // eslint-disable-next-line no-undef
+  StorageLocal = browser.storage.local
+} catch (e) {
+  // eslint-disable-next-line no-undef
+  const Chrome = chrome
+  StorageLocal = {
+    get: () => {
+      return new Promise((resolve, reject) => {
+        Chrome.storage.local.get(null, (items) => {
+          if (Chrome.runtime.lastError) {
+            return reject(Chrome.runtime.lastError)
+          }
+          resolve(items)
+        })
+      })
+    },
+    set: (data) => {
+      return new Promise((resolve, reject) => {
+        Chrome.storage.local.set(data, (items) => {
+          if (Chrome.runtime.lastError) {
+            return reject(Chrome.runtime.lastError)
+          }
+          resolve(items)
+        })
+      })
+    }
+  }
+}
+
+try {
+  const styleKey = 'wv3ta1_shrink'
+  const style = document.createElement('style')
+  // language=CSS
+  style.innerHTML = `
+        .${styleKey} {
+            opacity: 0.35 !important;
+            font-size: 12px !important;
+            padding: 1px 0px 1px 8px !important
+        }
+
+        .${styleKey} .instance-block {
+            display: flex;
+        }
+
+        .${styleKey} .instance-block img {
+            height: 16px;
+        }
+    `
+  document.body.appendChild(style);
+
+  (async () => {
+    const significantInstances = (await StorageLocal.get())?.significantInstances ?? {}
+    const intervalId = setInterval(() => {
+      if (!document.querySelectorAll('portal-application').length) {
+        // Retry until app found
+      } else {
+        Array.from(document.querySelectorAll('portal-application')).forEach(app => {
+          app.addEventListener('click', () => {
+            Array.from(document.querySelectorAll('portal-instance')).forEach(instance => {
+              if (significantInstances[instance.id]) {
+                instance.classList.remove(styleKey)
+              } else {
+                instance.classList.add(styleKey)
+              }
+              const button = document.createElement('button')
+              button.className = 'myDeleteButton'
+              button.innerText = '@'
+              button.style = `
+                                position: absolute;
+                                top: ${instance.offsetTop};
+                                left: -24px;
+                            `
+              button.addEventListener('click', (event) => {
+                event.preventDefault()
+                if (significantInstances[instance.id]) {
+                  instance.classList.add(styleKey)
+                  delete significantInstances[instance.id]
+                } else {
+                  instance.classList.remove(styleKey)
+                  significantInstances[instance.id] = true
+                }
+                StorageLocal.set({
+                  significantInstances
+                })
+              })
+              instance.parentElement.insertBefore(button, instance)
+            })
+          })
+        })
+        clearInterval(intervalId)
+      }
+    }, 50)
+  })()
+} catch (e) {
+  console.error(e)
+}
